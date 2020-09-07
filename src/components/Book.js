8cb796bb4d198187a51b14454344699d710aa097
Luis <luisglopez7777@gmail.com>
2020-09-06 23:04:04 -0500

    Errors to be corrected

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Row, Col, UncontrolledDropdown, DropdownMenu, DropdownItem, Inout, Buttom } from 'reactstrap'
+import { Row, Col, UncontrolledDropdown, DropdownMenu, DropdownItem, Input, Button, DropdownToggle } from 'reactstrap'
 import Table from './Table'
 
 const Book = (props) => {
@@ -10,7 +10,7 @@ const Book = (props) => {
     const [selection, setSelection] = useState({
         table: {
             name: null,
-            id: null
+            id: null,
         },
         date: new Date(),
         time: null,
@@ -26,17 +26,49 @@ const Book = (props) => {
     })
 
     //Potential Locations
-    const [locations] = useState(["Any Location", "Patio", "Inside", "Bar"])
-    const [times] = useState(["9AM", "10AM", "11AM", "1PM", "2PM", "3PM", "4PM", "5PM"])
+    const [locations] = useState([
+        "Any Location",
+        "Patio",
+        "Inside",
+        "Bar"])
+
+    const [times] = useState([
+        "9AM",
+        "10AM",
+        "11AM",
+        "12PM",
+        "1PM",
+        "2PM",
+        "3PM",
+        "4PM",
+        "5PM"])
 
     //Basic reservation validation
     const [reservationError, setReservationError] = useState(false)
 
     const getDate = () => {
-        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-        const date = months[selection.date.getMonth()] + " " + selection.date.getDate() + " " + selection.date.getFullYear()
-        let time = selection.time > 12 ? time + 12 + ":00" : time + ":00"
+        const months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"]
+
+        const date = months[selection.date.getMonth()] + " "
+            + selection.date.getDate() + " " + selection.date.getFullYear()
+
+
+        let time = selection.time.slice(0, -2);
+        time = selection.time > 12 ? time + 12 + ":00" : time + ":00"
         console.log(time)
+
         const datetime = new Date(date + " " + time)
         return datetime
     }
@@ -44,20 +76,20 @@ const Book = (props) => {
     const getEmptyTables = () => {
         let tables = totalTables.filter(table => table.isAvailable)
         return tables.length
-    }
+    };
 
     useEffect(() => {
         //Check availability of tables in DB
         if (selection.time && selection.date) {
             (async () => {
-                let dateTime = getDate()
+                let datetime = getDate()
                 let res = await fetch('http://localhost:3005/availability', {
                     method: 'POST',
                     headers: {
                         "Content-Type": "application/json"
                     },
                     body: JSON.stringify({
-                        date: dateTime
+                        date: datetime
                     })
                 })
                 res = await res.json()
@@ -65,14 +97,14 @@ const Book = (props) => {
                 let tables = res.tables.filter(
                     table =>
                         (selection.size > 0 ? table.capacity >= selection.size : true) &&
-                        (selection.location !== "Any location"
-                            ? table.location === selection.location : true)
+                        (selection.location !== "Any Location" ? table.location === selection.location : true)
 
                 )
                 setTotalTables(tables)
             })()
         }
-    }, [selection.time, selection.date, selection.size, selection.location])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selection.time, selection.date, selection.size, selection.location]);
 
     // Make reservation if all filters are filled out
     const reserve = async () => {
@@ -81,27 +113,28 @@ const Book = (props) => {
             booking.phone.length === 0 |
             booking.email.length === 0
         ) {
-            console.log('Incomplete details')
+            console.log('Incomplete Details')
             setReservationError(true)
         } else {
             const datetime = getDate()
-            let res = await fetch('http://localhost:3005', {
+            let res = await fetch('http://localhost:3005/reserve', {
                 method: 'POST',
                 headers: {
-                    "Contet-Type": "application/json",
-                    body: JSON.stringify({
-                        ...booking,
-                        date: datetime,
-                        table: selection.table.id
-                    })
-                }
+                    "Contet-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    ...booking,
+                    date: datetime,
+                    table: selection.table.id
+                })
             })
             res = await res.text()
-            console.log('Reserved ' + res)
+            console.log('Reserved: ' + res)
             props.setPage(2)
         }
     }
 
+    // Clicking on a table sets the selection state
     const selectTable = (table_name, table_id) => {
         setSelection({
             ...selection,
@@ -181,7 +214,7 @@ const Book = (props) => {
 
     const getTables = () => {
         console.log('Getting Tables')
-        if (getEmptyTables > 0) {
+        if (getEmptyTables() > 0) {
             let tables = []
             totalTables.forEach(table => {
                 if (table.isAvailable) {
@@ -209,7 +242,7 @@ const Book = (props) => {
             })
             return tables
         }
-    }
+    };
 
 
     return (
@@ -218,14 +251,169 @@ const Book = (props) => {
                 <Col>
                     <p className="looking-for-pizza">
                         {!selection.table.id ? "Book a table" : "Confirm reservation"}
-                        <i></i>
+                        <i className={!selection.table.id
+                            ? "fas fa-chair pizza-slice"
+                            : "fas fa-clipboard pizza-slice"}>
+                        </i>
                     </p>
+
+                    <p className="selected-table">
+                        {selection.table.id ? "You are booking a table " + selection.table.name : null}
+                    </p>
+                    {reservationError ? (
+                        <p className="reservation-error">
+                            *Please fill all the details
+                        </p>
+                    ) : null}
                 </Col>
             </Row>
+
+            {!selection.table.id ? (
+                <div id="reservation-stuff">
+                    <Row noGutters className="text-center align-items-center">
+                        <Col xs="12" sm="3">
+                            <input type="date" required="required" className="booking-dropdown"
+                                value={selection.date.toISOString().split("T")[0]}
+                                onChange={(e) => {
+                                    if (!isNaN(new Date(new Date(e.target.value)))) {
+                                        let newSel = {
+                                            ...selection,
+                                            table: {
+                                                ...selection.table
+                                            },
+                                            date: new Date(e.target.value)
+                                        }
+                                        setSelection(newSel)
+                                    } else {
+                                        console.log('Invalid Date')
+                                        let newSel = {
+                                            ...selection,
+                                            table: {
+                                                ...selection.table
+                                            },
+                                            date: new Date()
+                                        }
+                                        setSelection(newSel)
+                                    }
+                                }}>
+                            </input>
+                        </Col>
+
+                        <Col xs="12" sm="3">
+                            <UncontrolledDropdown>
+                                <DropdownToggle color="none" caret className="booking-dropdown">
+                                    {selection.time === null ? "Select a time" : selection.time}
+                                </DropdownToggle>
+
+                                <DropdownMenu right className="booking-dropdown-menu">
+                                    {getTimes()}
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Col>
+
+                        <Col xs="12" sm="3">
+                            <UncontrolledDropdown>
+                                <DropdownToggle color="none" caret className="booking-dropdown">
+                                    {selection.location}
+                                </DropdownToggle>
+
+                                <DropdownMenu right className="booking-dropdown-menu">
+                                    {getLocations()}
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Col>
+
+                        <Col xs="12" sm="3">
+                            <UncontrolledDropdown>
+                                <DropdownToggle color="none" caret className="booking-dropdown">
+                                    {selection.size === 0 ? "Select a Party Size" : selection.size.toString()}
+                                </DropdownToggle>
+
+                                <DropdownMenu right className="booking-dropdown-menu">
+                                    {getSizes()}
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Col>
+                    </Row>
+
+                    <Row noGutters className="tables-display">
+                        <Col>
+                            {getEmptyTables > 0 ? (
+                                <p className="available-tables">{getEmptyTables()} available</p>
+                            ) : null}
+
+                            {selection.date && selection.time ? (
+                                getEmptyTables() > 0 ? (
+                                    <div>
+                                        <div className="table-key">
+                                            <span className="empty-table">&nbsp; Available &nbsp;&nbsp;</span>
+                                            <span className="full-table">&nbsp; Unavailable &nbsp;&nbsp;</span>
+                                        </div>
+                                        <Row noGutters>{getTables()}</Row>
+                                    </div>
+                                ) : (
+                                        <p className="table-display-message">No Available Tables</p>
+                                    )
+                            ) : (
+                                    <p className="table-display-message">
+                                        Please select a date and time for your reservation
+                                    </p>
+                                )
+                            }
+                        </Col>
+
+                    </Row>
+                </div>
+            ) : (
+
+                    <div id="confirm-reservation-stuff">
+                        <Row noGutters className="text-center justify-content-center reservation-details-container">
+                            <Col xs="12" sm="3" className="reservation-details">
+                                <Input type="text" bsSize="lg" placeholder="Name" className="reservation-input"
+                                    value={booking.name} onChange={e => {
+                                        setBooking({
+                                            ...booking,
+                                            name: e.target.value
+                                        })
+                                    }} />
+                            </Col>
+
+                            <Col xs="12" sm="3" className="reservation-details">
+                                <Input type="text" bsSize="lg" placeholder="Phone Number" className="reservation-input"
+                                    value={booking.phone} onChange={e => {
+                                        setBooking({
+                                            ...booking,
+                                            phone: e.target.value
+                                        })
+                                    }} />
+                            </Col>
+
+
+                            <Col xs="12" sm="3" className="reservation-details">
+                                <Input type="text" bsSize="lg" placeholder="Email" className="reservation-input"
+                                    value={booking.email} onChange={e => {
+                                        setBooking({
+                                            ...booking,
+                                            email: e.target.value
+                                        })
+                                    }} />
+                            </Col>
+
+                        </Row>
+                        <Row noGutters className="text-center">
+                            <Col>
+                                <Button color="none" className="book-table-btn" onClick={() => {
+                                    reserve()
+                                }}>
+                                    Book Now
+                    </Button>
+                            </Col>
+                        </Row>
+
+                    </div>
+                )}
         </div>
     )
-
 }
-
 
 export default Book
